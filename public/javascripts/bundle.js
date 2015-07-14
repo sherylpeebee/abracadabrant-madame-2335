6fd89ee7087b58043d25a49e4c6a3b2fa5f0c02a
warlyware <danward@gmail.com>
2015-07-14 09:48:47 -0700

    Add routes to user info in nav

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -197,13 +197,20 @@ angular.module('GriftrApp')
 'use strict()';
 
 angular.module('GriftrApp')
-.controller('NavCtrl', function($scope, $http, $rootScope) {
+.controller('NavCtrl', function($scope, $http, $rootScope, $state) {
   console.log('nav ctrl');
   $http.get("/getUserData").success(function(userData){
     console.log(userData);
     $rootScope.currentUser = userData;
   });
   $scope.goToProfile = function() {
+    if ($rootScope.currentUser.owner) {
+      console.log('owner');
+      $state.go('info.owner');
+    } else {
+      console.log('traveller');
+      $state.go('info.traveller');
+    }
     console.log($rootScope.currentUser);
   }
 });
